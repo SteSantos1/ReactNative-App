3c054a4e5ef879c2297c60a49a13bbe94ec83bab
SteSantos1 <stephanierocha842@gmail.com>
2025-08-19 07:53:03 -0300

    montando a primeira tela inicial, colocando cores e arrumando estilizacao da tela

--- src/global/login/index.tsx
+++ src/global/login/index.tsx
@@ -1,27 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
-import {style} from './styles';
-import Logo from "../../assets/logo.png";
 import React from 'react';
- 
+import { Text, View, Image, TextInput } from 'react-native';
+import { style } from './styles';
+import Logo from "../../assets/logo.png";
+
 export default function Login() {
   return (
     <View style={style.container}>
       <View style={style.boxTop}>
-        <Text>Top</Text>
-        <Image source={Logo}/>
+        <Image
+          source={Logo}
+          style={style.logo}
+          resizeMode="contain"
+        />
+        <Text style={style.text}>Bem-Vindo de Volta</Text>
       </View>
       <View style={style.boxMid}>
-        <Text>Mid</Text>
-        <Text>Bem vindo de Volta!</Text>
-        <TextInput />
-        <Text>Senha</Text>
-        <TextInput />
+        <Text style={style.titleInput}>Endereço de E-mail</Text>
+        <View style={style.boxInput}>
+          <TextInput
+            style={style.input}
+          />
+          <Text>Olá</Text>
         </View>
-        <View style={style.boxBotton}>
-          <Text>Botton</Text>
-          <Text>Endereço de E-mail</Text>
-        </View>
-    </View>  
+        <Text style={style.titleInput}>Senha</Text>
+        <TextInput />
+      </View>
+      <View style={style.boxBotton}>
+      </View>
+    </View>
   );
 }
