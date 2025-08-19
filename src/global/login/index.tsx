23b0dc4ba1cd2cefd03b3211a40e73422e80782f
SteSantos1 <stephanierocha842@gmail.com>
2025-08-19 09:04:31 -0300

    colocando icones, baixando biblioteca dos icones e arrumando o input e a estilização

--- src/global/login/index.tsx
+++ src/global/login/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Text, View, Image, TextInput } from 'react-native';
 import { style } from './styles';
 import Logo from "../../assets/logo.png";
+import { MaterialIcons } from '@expo/vector-icons'
+import { themas } from "../../global/themes"
 
 export default function Login() {
   return (
@@ -12,17 +14,31 @@ export default function Login() {
           style={style.logo}
           resizeMode="contain"
         />
-        <Text style={style.text}>Bem-Vindo de Volta</Text>
+        <Text style={style.text}>Bem-Vindo de Volta!</Text>
       </View>
       <View style={style.boxMid}>
-        <Text style={style.titleInput}>Endereço de E-mail</Text>
+        <Text style={style.titleInput}>ENDEREÇO DE E-MAIL</Text>
         <View style={style.boxInput}>
           <TextInput
             style={style.input}
           />
-          <Text>Olá</Text>
+          <MaterialIcons 
+          name = "email"
+          size={20}
+          color={themas.colors.gray}
+          />
         </View>
-        <Text style={style.titleInput}>Senha</Text>
+        <Text style={style.titleInput}>SENHA</Text>
+        <View style={style.boxInput}>
+          <TextInput
+            style={style.input}
+          />
+          <MaterialIcons 
+          name="remove-red-eye"
+          size={20}
+          color={themas.colors.gray}
+          />
+        </View>
         <TextInput />
       </View>
       <View style={style.boxBotton}>
