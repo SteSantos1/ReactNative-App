3a3e8a177869fa716831b7133a66789c60a174ef
SteSantos1 <stephanierocha842@gmail.com>
2025-09-01 16:58:16 -0300

    Arrumando o codigo, utilizando as ferramentas com os componentes e atualizando a tela

--- src/componentes/input/style.tsx
+++ src/componentes/input/style.tsx
@@ -1,47 +1,8 @@
 import { StyleSheet } from "react-native";
-import { Dimensions } from "react-native";
-import { themes } from "../../global/themes"
+import { themes } from "../../global/themes";
 
 export const style = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        //padding: 20
-    },
-    boxTop: {
-        height: Dimensions.get('window').height / 3,
-        width: '100%',
-        //backgroundColor: 'red'
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    boxMid: {
-        height: Dimensions.get('window').height / 4,
-        width: '100%',
-        //backgroundColor: 'green'
-        paddingHorizontal: 37
-    },
-    boxBotton: {
-        height: Dimensions.get('window').height / 3,
-        width: '100%',
-        //backgroundColor: 'blue'
-        alignItems: 'center',
-    },
-    logo: {
-        width: 80,
-        height: 80
-    },
-    text: {
-        fontWeight: 'bold',
-        marginTop: 48,
-        fontSize: 18
-    },
-    titleInput: {
-        marginLeft: 5,
-        color: themes.colors.gray,
-        marginTop: 20
-    },
+
     boxInput: {
         width: '100%',
         height: 40,
@@ -57,34 +18,12 @@ export const style = StyleSheet.create({
     input: {
         height: '100%',
         width: '90%',
-        // backgroundColor: 'red',
-        borderRadius: 40
-    },
-    button: {
-        width: 250,
-        height: 50,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: themes.colors.primary,
         borderRadius: 40,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 3,
-        },
-        shadowOpacity: 0.29,
-        shadowRadius: 4.65,
-        elevation: 7,
+        paddingLeft: 5,
     },
-    textButton: {
-        fontSize: 16,
-        color: '#FFF',
-        fontWeight: 'bold'
+    titleInput: {
+        marginLeft: 5,
+        color: themes.colors.gray,
+        marginTop: 20
     },
-    textBotton: {
-        fontSize: 16,
-        color: themes.colors.gray
-    },
-
-
 })
