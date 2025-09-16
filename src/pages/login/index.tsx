11094c7f099817d9eec4f472a86e701f1f256d33
SteSantos1 <stephanierocha842@gmail.com>
2025-09-16 09:00:12 -0300

    finalizando o bottom navigation, deixando os botoes com opacidade ao clicar, atribuindo metodo de voltar de forma mais segura

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -28,7 +28,7 @@ export default function Login() {
                 return Alert.alert('Atenção', 'Informe os campos obrigatórios!');
             }
 
-            navigation.navigate("BottomRoutes")
+            navigation.reset({routes: [{name: "BottomRoutes"}]})
 
             console.log("Logado com sucesso! :)")
 
