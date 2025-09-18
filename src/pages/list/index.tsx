fd4749b26d0e9d1709654f400be77f27ba9cde74
SteSantos1 <stephanierocha842@gmail.com>
2025-09-18 08:07:45 -0300

    criando o header da lista, adicionando estilos, text e style para header

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { style } from "./styles";
+import { Input } from "../../componentes/input";
+import { MaterialIcons } from '@expo/vector-icons'
 
 export default function List() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Lista</Text>
+    <View style={style.container}>
+      <View style={style.header}>
+        <Text style={style.greeting}>Bom dia, <Text style={{ fontWeight: 'bold' }}>Stéphanie Santos!</Text></Text>
+        <View style={style.boxInput}>
+          <Input
+            IconLeft={MaterialIcons}
+            IconLeftName="search"
+          />
+        </View>
+      </View>
     </View>
   )
 }
