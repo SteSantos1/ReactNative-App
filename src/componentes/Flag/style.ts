4843f83bc2d42a9d01b002813d2e52e072235a80
SteSantos1 <stephanierocha842@gmail.com>
2025-09-22 12:16:19 -0300

    Criando cards e flags, para status dos proprios cards

---
+++ src/componentes/Flag/style.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+import { themas } from "../../global/themas";
+
+export const style = StyleSheet.create({
+    container: {
+        width: 60,
+        height: 30,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: themas.colors.red,
+        borderRadius: 4
+    }
+})
--- src/pages/list/styles.ts
+++ src/pages/list/styles.ts
@@ -1,15 +1,15 @@
 import { StyleSheet, Dimensions } from "react-native";
-import { themas } from "../../global/themas"
+import { themas } from "../../global/themas";
 
 export const style = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        //backgroundColor: 'red'
+        // backgroundColor: 'red'
     },
     header: {
         width: '100%',
-        height: Dimensions.get('window').height/6,
+        height: Dimensions.get('window').height / 6,
         backgroundColor: themas.colors.primary,
         paddingHorizontal: 20,
         justifyContent: 'center'
@@ -21,5 +21,34 @@ export const style = StyleSheet.create({
     },
     boxInput: {
         width: '80%'
+    },
+    boxList: {
+        flex: 1,
+        width: '100%',
+        // backgroundColor: 'red'
+    },
+    card: {
+        width: '100%',
+        height: 60,
+        backgroundColor: '#FFF',
+        marginTop: 6,
+        borderRadius: 10,
+        justifyContent: 'center',
+        padding: 10,
+        borderWidth: 1,
+        borderColor: themas.colors.lightGray
+    },
+    rowCard: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+
+    },
+    rowCardLeft: {
+        width: '70%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 10
     }
+
 })
